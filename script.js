13e9de1f19150533a7d22df9c95e2d2454667276
aniketjadhav1812 <128202035+aniketjadhav1812@users.noreply.github.com>
2023-04-07 22:50:37 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+var typed = new Typed('#typing-1', {
+    strings: ['Student', 'Coder','Developer'],
+    typeSpeed: 40,
+    backspeed:40,
+    loop:true,
+  });
+
+let menubtns = document.getElementsByClassName("menu-btn");
+console.log(menubtns);
+fo
